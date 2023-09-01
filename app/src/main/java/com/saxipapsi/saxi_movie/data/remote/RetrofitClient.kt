73a7c7b1dbb64cadb6a2f55716f184533d5d3e30
AzureAD\EricAlvarez <eric@migo.io>
2023-09-01 21:26:57 +0800

    secure api token

--- app/src/main/java/com/saxipapsi/saxi_movie/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/saxipapsi/saxi_movie/data/remote/RetrofitClient.kt
@@ -1,13 +1,28 @@
 package com.saxipapsi.saxi_movie.data.remote
 
+import com.saxipapsi.saxi_movie.BuildConfig
 import com.saxipapsi.saxi_movie.common.API
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+object AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", BuildConfig.API_KEY)
+            .addHeader("Content-Type", "application/json")
+            .build()
+        return chain.proceed(request)
+    }
+}
 
 val networkModule = module {
-    single { getRetrofit() }
+    single { AuthInterceptor }
+    single { getOkHttpClient(get()) }
+    single { getRetrofit(get()) }
     single { getTMDBApi(get()) }
     single { RetrofitClient(get()).initialize() }
 }
@@ -19,9 +34,17 @@ class RetrofitClient(private val retrofit: Retrofit) {
     }
 }
 
-private fun getRetrofit(): Retrofit {
+private fun getOkHttpClient(authInterceptor : AuthInterceptor): OkHttpClient{
+    val okHttpClient : OkHttpClient.Builder = OkHttpClient.Builder()
+    okHttpClient.addInterceptor(authInterceptor)
+    return okHttpClient.build()
+}
+
+
+private fun getRetrofit(okHttpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
         .baseUrl(API.TMDBApi.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
--- app/src/main/java/com/saxipapsi/saxi_movie/data/remote/TMDBApi.kt
+++ app/src/main/java/com/saxipapsi/saxi_movie/data/remote/TMDBApi.kt
@@ -8,22 +8,12 @@ import com.saxipapsi.saxi_movie.data.remote.dto.video.ContentVideoDto
 import retrofit2.http.*
 
 interface TMDBApi {
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.POPULAR_CONTENTS)
     suspend fun getPopularContents(
         @Path("content_type") content_type: String,
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentPageDto
 
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.CONTENT_DETAIL)
     suspend fun getContentDetails(
         @Path("content_id") contentId: Int,
@@ -31,34 +21,18 @@ interface TMDBApi {
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentDetailsDto
 
-
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.TRENDING_CONTENTS)
     suspend fun getTrendingContents(
         @Path("trend_type") trend_type: String,
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentPageDto
 
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.TOP_RATED_CONTENTS)
     suspend fun getTopRatedContents(
         @Path("content_type") content_type: String,
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentPageDto
 
-
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.CONTENT_VIDEOS)
     suspend fun getContentVideos(
         @Path("content_type") content_type: String,
@@ -66,28 +40,15 @@ interface TMDBApi {
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentVideoDto
 
-
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.UPCOMING_MOVIES)
     suspend fun getUpComingMovies(
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): ContentPageDto
 
-
-    /*TODO Token should be protected*/
-    @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjQxMDBkZTE4NDNjNGI0MDU1NWYxYTAxNTlkNDI3NiIsInN1YiI6IjY0ZDYwNmUyZjE0ZGFkMDBhZDRlM2UzMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MJHaw0MmmcOlW1hbLacBsF1FBXagszr-RQfYQW3gYZ8",
-        "Content-Type: application/json"
-    )
     @GET(API.TMDBApi.CREDITS)
     suspend fun getContentCredits(
         @Path("content_type") content_type: String,
         @Path("content_id") content_id: Int,
         @Query("api_key") api_key: String = "7f4100de1843c4b40555f1a0159d4276"
     ): CreditsDto
-
 }
